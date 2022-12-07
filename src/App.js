dd6d9a5a1e6e3ab57ff926543b02c5a4878222cd
Ahmed Essam <bloxed7@gmail.com>
2022-12-07 17:08:32 +0200

    project layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Layout } from "./components";
 import { Homepage } from "./pages";
 function App() {
     return (
         <Layout>
-            <Homepage />
+            <BlogContext>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/:genre" element={<Genrepage />} />
+                        <Route path="/:genre/:blog" element={<Blogpage />} />
+                    </Routes>
+                </Router>
+                <Homepage />
+            </BlogContext>
         </Layout>
     );
 }
